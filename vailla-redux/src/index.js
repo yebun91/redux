5ed697c14635a76ec3b8ead1bd298648ec5733c9
DESKTOP-K9E80N0\dnstj <yebun91@gmail.com>
2022-05-23 21:21:16 +0900

    no message

--- vailla-redux/src/index.js
+++ vailla-redux/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const countModifier = (count = 0, action) => {
+	if (action.type === 'ADD') {
+		count++;
+	} else if (action.type === 'MINUS') {
+		count--;
+	}
+	return count;
+};
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
+
+countStore.dispatch({ type: 'ADD' });
+add.addEventListener('click', countStore.dispatch({ type: 'ADD' }));
+minus.addEventListener('click', countStore.dispatch({ type: 'MINUS' }));
